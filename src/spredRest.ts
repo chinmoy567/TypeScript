e2a82a9ce4d3c521f4e73dc0e39ae9c22a76cb02
Chinmoy Sarkar <chinmoy4907@gmail.com>
2026-05-08 02:50:38 +0600

    git clone

---
+++ src/spredRest.ts
@@ -0,0 +1,57 @@
+// tsx src/spredRest.ts
+
+// const friends = [
+// 	"Chinmoy",
+// 	"Sourav",
+// 	"Aman",
+// 	"Riya",
+// 	"Priya",
+// 	"Arjun",
+// 	"Karan",
+// 	"Neha",
+// 	"Ankit",
+// 	"Rohan",
+// ];
+
+// const schoolFriends = [
+// 	...friends.slice(0, 5),
+// 	"Rahul",
+// 	"Sunita",
+// 	"Ramesh",
+// 	"Tanya",
+// 	"Vikram",
+// ];
+
+// const collegeFriends = [
+// 	...friends.slice(5),
+// 	"Kabir",
+// 	"Manisha",
+// 	"Siddharth",
+// 	"Pooja",
+// 	"Ishaan",
+// ];
+
+// console.log("friends:", friends);
+// console.log("schoolFriends:", schoolFriends);
+// console.log("collegeFriends:", collegeFriends);
+
+
+// tsx src/spredRest.ts
+// const  user ={
+//     name: "Chinmoy",
+//     age: 25,
+//     city: "cumilla",
+//     profession: "Software Engineer",
+// }
+
+// const user_qualifications ={
+//     degree: "BSc in Computer Science",
+//     university: "Baiust",
+//     graduationYear: 2026,
+// }
+// const user_details = {
+//     ...user,
+//     ...user_qualifications,
+// }   
+// console.log(user_details);
+
