79c2091af6d37ba84e6ff590efb04289adce3957
Chinmoy Sarkar <chinmoy4907@gmail.com>
2026-05-08 19:57:10 +0600

    Remove unused test files and source maps from the dist and src directories

--- dist/test.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const course = "TypeScript for Beginners";
-console.log(`Welcome to the course: ${course}`);
-//# sourceMappingURL=test.js.map
