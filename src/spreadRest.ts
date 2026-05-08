d4800ffeb5e2bbca3f31381d21ceb1532fcbde3d
Chinmoy Sarkar <chinmoy4907@gmail.com>
2026-05-08 18:42:47 +0600

    Refactor spreadRest.ts to add friends and invitation functionality; remove unused user details

--- src/spreadRest.ts
+++ src/spreadRest.ts
@@ -37,21 +37,60 @@
 
 
 // tsx src/spreadRest.ts
-const  user ={
-    name: "Chinmoy",
-    age: 25,
-    city: "cumilla",
-    profession: "Software Engineer",
-}
+// const  user ={
+//     name: "Chinmoy",
+//     age: 25,
+//     city: "cumilla",
+//     profession: "Software Engineer",
+// }
 
-const user_qualifications ={
-    degree: "BSc in Computer Science",
-    university: "Baiust",
-    graduationYear: 2026,
-}
-const user_details = {
-    ...user,
-    ...user_qualifications,
+// const user_qualifications ={
+//     degree: "BSc in Computer Science",
+//     university: "Baiust",
+//     graduationYear: 2026,
+// }
+// const user_details = {
+//     ...user,
+//     ...user_qualifications,
+// }   
+// console.log(user_details);
+
+
+// tsx src/spreadRest.ts
+
+const friends = [
+	"Chinmoy",
+	"Sourav",
+	"Aman",
+	"Riya",
+	"Priya",
+	"Arjun",
+	"Karan",
+	"Neha",
+	"Ankit",
+	"Rohan",
+];
+
+const schoolFriends = [
+    ...friends.slice(0, 5),
+	"Rahul",
+	"Sunita",
+	"Ramesh",
+	"Tanya",
+	"Vikram",
+];
+
+const collegeFriends = [
+    ...friends.slice(5),
+	"Kabir",
+	"Manisha",
+	"Siddharth",
+	"Pooja",
+	"Ishaan",
+];
+const sendInvite=(...friends:string[])=>{
+    friends.forEach(friend=>{
+        console.log(`Invitation sent to ${friend}`);
+    })
 }   
-console.log(user_details);
-
+sendInvite(...friends);
