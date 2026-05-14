5ebe6b13d31d561bd866a592c4f4f449eec7d673
Chinmoy Sarkar <chinmoy4907@gmail.com>
2026-05-14 19:21:20 +0600

    Add class and access modifiers examples in TypeScript

---
+++ src/Class and object.ts
@@ -0,0 +1,58 @@
+
+// class Chai {
+//     flavour: string;
+//     price: number;
+
+//     constructor(flavour: string, price: number) {
+//         this.flavour = flavour;
+//         this.price = price;
+//     }
+// }
+
+
+// // Create Object
+// const masalaChai = new Chai("Masala", 20);
+
+// console.log(masalaChai.flavour);
+// console.log(masalaChai.price);
+
+
+
+
+
+// // ========================================
+// // ACCESS MODIFIERS
+// // ========================================
+
+// class User {
+
+//     public name: string;
+//     private password: string;
+//     protected age: number;
+
+//     constructor(name: string, password: string, age: number) {
+//         this.name = name;
+//         this.password = password;
+//         this.age = age;
+//     }
+
+//     showPassword() {
+//         console.log(this.password);
+//     }
+// }
+
+// const user1 = new User("Sourav", "1234", 22);
+
+// console.log(user1.name);
+
+// user1.showPassword();
+
+// // Error
+// // console.log(user1.password);
+
+// // Error
+// // console.log(user1.age);
+
+
+
+
